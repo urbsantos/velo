f534f22fa5d81c066b51d4690c93b3e7e78bea98
Urbano Santos <urbanolopes@gmail.com>
2026-09-24 16:18:45 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  //Checkpoint 1: Verificar se a página está carregada
+ //Arrange
+  await page.goto('http://localhost:5173/');  
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint 2: Verificar se a página de consulta de pedido está carregada
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-8NWAW7');
   await page.getByTestId('search-order-button').click();
-  
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-8NWAW7');
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-
 });
